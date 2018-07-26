3a7b6e2b838d8ed22778b7bef8960db2004fe983
Jason Kirn <jkirn@csumb.edu>
2018-07-26 22:25:43 +0000

    initial working copy of TicTacToe game

--- JSPractice/JSPractice/TicTacToe/js/ticTacToe.js
+++ JSPractice/JSPractice/TicTacToe/js/ticTacToe.js
@@ -5,14 +5,15 @@ $("#ticTacToeImg").click( function() {
 function checkForWin() {
     
 }
-var turn = "Xturn";
+
 var currentTurn = turn =>  {
-    if (turn === "Xturn") {
-        turn = "Oturn";
+    console.log("Turn is: " + this.turn);
+    if (this.turn === "Xturn") {
+        this.turn = "Oturn";
         return "O";
     }
     else {
-        turn = "Xturn";
+        this.turn = "Xturn";
         return "X";
     }
 }
@@ -21,47 +22,47 @@ $("#area1Id").click( function() {
     console.log("Area 1 clicked");
     //$("#ticTacToeImg").append("<img id='circleImg' src='images/O.png' style='position: absolute;' alt='circleImg'/>");
     //$("#imgArea1").append("<img id='circleImg' src='images/O.png' alt='circleImg'/>");
-    currentTurn();
-    console.log(currentTurn());
+    //currentTurn();
+    //console.log(currentTurn());
     $("#ticTacToeBoard").append("<img id='area1ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
 });
 
 $("#area2Id").click( function() {
     console.log("Area 2 clicked");
-    $("#ticTacToeBoard").append("<img id='area2ImageSpot' src='images/O.png' alt='circleImg'/>");
+    $("#ticTacToeBoard").append("<img id='area2ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
 });
 
 $("#area3Id").click( function() {
     console.log("Area 3 clicked");
-    $("#ticTacToeBoard").append("<img id='area3ImageSpot' src='images/O.png' alt='circleImg'/>");
+    $("#ticTacToeBoard").append("<img id='area3ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
 });
 
 $("#area4Id").click( function() {
     console.log("Area 4 clicked");
-    $("#ticTacToeBoard").append("<img id='area4ImageSpot' src='images/O.png' alt='circleImg'/>");
+    $("#ticTacToeBoard").append("<img id='area4ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
 });
 
 $("#area5Id").click( function() {
     console.log("Area 5 clicked");
-    $("#ticTacToeBoard").append("<img id='area5ImageSpot' src='images/O.png' alt='circleImg'/>");
+    $("#ticTacToeBoard").append("<img id='area5ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
 });
 
 $("#area6Id").click( function() {
     console.log("Area 6 clicked");
-    $("#ticTacToeBoard").append("<img id='area6ImageSpot' src='images/O.png' alt='circleImg'/>");
+    $("#ticTacToeBoard").append("<img id='area6ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
 });
 
 $("#area7Id").click( function() {
     console.log("Area 7 clicked");
-    $("#ticTacToeBoard").append("<img id='area7ImageSpot' src='images/O.png' alt='circleImg'/>");
+    $("#ticTacToeBoard").append("<img id='area7ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
 });
 
 $("#area8Id").click( function() {
     console.log("Area 8 clicked");
-    $("#ticTacToeBoard").append("<img id='area8ImageSpot' src='images/O.png' alt='circleImg'/>");
+    $("#ticTacToeBoard").append("<img id='area8ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
 });
 
 $("#area9Id").click( function() {
     console.log("Area 9 clicked");
-    $("#ticTacToeBoard").append("<img id='area9ImageSpot' src='images/O.png' alt='circleImg'/>");
+    $("#ticTacToeBoard").append("<img id='area9ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
 });
