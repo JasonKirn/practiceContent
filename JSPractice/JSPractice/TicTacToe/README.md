75eed4c9e6948d8f475a1af49d6fad859e310c78
Jason Kirn <jkirn@csumb.edu>
2018-07-27 00:41:36 +0000

    Game progress now tracked in 2d array

---
+++ JSPractice/JSPractice/TicTacToe/README.md
@@ -0,0 +1,11 @@
+Custom TicTacToe game that uses a mixture of ES5 and ES6 Javascript.
+
+Uses babel standalone script to transpilate any ES6 to ES5.
+
+Game works through mapping areas on the TicTacToe board image to create individual areas
+that then hold X or O when pressed.
+
+Current turn is tracked with a fat arrow function and is changed after every X or O
+is placed.
+
+Gameprogress is tracked in a 2d array to watch for win condition.
--- JSPractice/JSPractice/TicTacToe/js/ticTacToe.js
+++ JSPractice/JSPractice/TicTacToe/js/ticTacToe.js
@@ -1,3 +1,9 @@
+var gameProgress = [
+        ["","",""],
+        ["","",""],
+        ["","",""]
+    ];
+
 $("#ticTacToeImg").click( function() {
     alert("Clicked"); 
 });
@@ -6,6 +12,7 @@ function checkForWin() {
     
 }
 
+//turn assigns itself once first move is placed
 var currentTurn = turn =>  {
     console.log("Turn is: " + this.turn);
     if (this.turn === "Xturn") {
@@ -20,49 +27,73 @@ var currentTurn = turn =>  {
 
 $("#area1Id").click( function() {
     console.log("Area 1 clicked");
-    //$("#ticTacToeImg").append("<img id='circleImg' src='images/O.png' style='position: absolute;' alt='circleImg'/>");
-    //$("#imgArea1").append("<img id='circleImg' src='images/O.png' alt='circleImg'/>");
-    //currentTurn();
-    //console.log(currentTurn());
-    $("#ticTacToeBoard").append("<img id='area1ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
+    var contentToFillSquare = currentTurn();
+    $("#ticTacToeBoard").append("<img id='area1ImageSpot' src='images/" + contentToFillSquare + ".png' alt='circleImg'/>");
+    gameProgress[0][0] = contentToFillSquare;
+    console.log("Filling square 1 with: " + gameProgress[0][0]);
+    
 });
 
 $("#area2Id").click( function() {
     console.log("Area 2 clicked");
-    $("#ticTacToeBoard").append("<img id='area2ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
+    var contentToFillSquare = currentTurn();
+    $("#ticTacToeBoard").append("<img id='area2ImageSpot' src='images/" + contentToFillSquare + ".png' alt='circleImg'/>");
+    gameProgress[0][1] = contentToFillSquare;
+    console.log("Filling square 2 with: " + gameProgress[0][1]);
 });
 
 $("#area3Id").click( function() {
     console.log("Area 3 clicked");
-    $("#ticTacToeBoard").append("<img id='area3ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
+    var contentToFillSquare = currentTurn();
+    $("#ticTacToeBoard").append("<img id='area3ImageSpot' src='images/" + contentToFillSquare + ".png' alt='circleImg'/>");
+    gameProgress[0][2] = contentToFillSquare;
+    console.log("Filling square 3 with: " + gameProgress[0][2]);
 });
 
 $("#area4Id").click( function() {
     console.log("Area 4 clicked");
-    $("#ticTacToeBoard").append("<img id='area4ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
+    var contentToFillSquare = currentTurn();
+    $("#ticTacToeBoard").append("<img id='area4ImageSpot' src='images/" + contentToFillSquare + ".png' alt='circleImg'/>");
+    gameProgress[1][0] = contentToFillSquare;
+    console.log("Filling square 4 with: " + gameProgress[1][0]);
 });
 
 $("#area5Id").click( function() {
     console.log("Area 5 clicked");
-    $("#ticTacToeBoard").append("<img id='area5ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
+    var contentToFillSquare = currentTurn();
+    $("#ticTacToeBoard").append("<img id='area5ImageSpot' src='images/" + contentToFillSquare + ".png' alt='circleImg'/>");
+    gameProgress[1][1] = contentToFillSquare;
+    console.log("Filling square 5 with: " + gameProgress[1][1]);
 });
 
 $("#area6Id").click( function() {
     console.log("Area 6 clicked");
-    $("#ticTacToeBoard").append("<img id='area6ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
+    var contentToFillSquare = currentTurn();
+    $("#ticTacToeBoard").append("<img id='area6ImageSpot' src='images/" + contentToFillSquare + ".png' alt='circleImg'/>");
+    gameProgress[1][2] = contentToFillSquare;
+    console.log("Filling square 6 with: " + gameProgress[1][2]);
 });
 
 $("#area7Id").click( function() {
     console.log("Area 7 clicked");
-    $("#ticTacToeBoard").append("<img id='area7ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
+    var contentToFillSquare = currentTurn();
+    $("#ticTacToeBoard").append("<img id='area7ImageSpot' src='images/" + contentToFillSquare + ".png' alt='circleImg'/>");
+    gameProgress[2][0] = contentToFillSquare;
+    console.log("Filling square 7 with: " + gameProgress[2][0]);
 });
 
 $("#area8Id").click( function() {
     console.log("Area 8 clicked");
-    $("#ticTacToeBoard").append("<img id='area8ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
+    var contentToFillSquare = currentTurn();
+    $("#ticTacToeBoard").append("<img id='area8ImageSpot' src='images/" + contentToFillSquare + ".png' alt='circleImg'/>");
+    gameProgress[2][1] = contentToFillSquare;
+    console.log("Filling square 8 with: " + gameProgress[2][1]);
 });
 
 $("#area9Id").click( function() {
     console.log("Area 9 clicked");
-    $("#ticTacToeBoard").append("<img id='area9ImageSpot' src='images/" + currentTurn() + ".png' alt='circleImg'/>");
+    var contentToFillSquare = currentTurn();
+    $("#ticTacToeBoard").append("<img id='area9ImageSpot' src='images/" + contentToFillSquare + ".png' alt='circleImg'/>");
+    gameProgress[2][2] = contentToFillSquare;
+    console.log("Filling square 9 with: " + gameProgress[2][2]);
 });
